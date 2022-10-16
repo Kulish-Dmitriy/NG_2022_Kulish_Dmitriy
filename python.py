9b64784de9ea3ce3ab43c186080b31382226db64
Kulish-Dmitriy <115895460+Kulish-Dmitriy@users.noreply.github.com>
2022-10-16 14:19:52 +0400

    Add files via upload

---
+++ python.py
@@ -0,0 +1,16 @@
+what=input("Whats do we do (+),(-).(/),(*)")
+number1=float(input("number1:"))
+number2=float(input("number2:"))
+if what == "+":
+    c=number1+number2
+    print(c)
+if what=="-":
+    c=number1-number2
+    print(c)
+if what=="/":
+    c=number1/number2
+    print(c)
+if what=="*":
+    c=number1*number2
+    print(c)
+
